a24e27352c0994a71ecfad86671db88d09f9ce42
Daniel Simango <daniel.simmz@gmail.com>
2020-05-19 16:14:16 -0500

    done

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const employees = [
     name: 'Atticus',
     employeeNumber: '2405',
     annualSalary: '47000',
-    reviewRating: 3
+    reviewRating: 2
   },
   {
     name: 'Jem',
@@ -40,4 +40,50 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+function bonusCalculator(employee) {
+  let objOut = {
+    name: employee.name,
+    bonusPercentage: 0,
+    totalCompensation: 0,
+    totalBonus: 0,
+  };
+
+  let bonus = 0;
+  if (employee.reviewRating === 5) {
+    console.log("nice job, 10%");
+    bonus = 0.1;
+  }
+  if (employee.reviewRating === 4) {
+    console.log("good job, 6%");
+    bonus = 0.06;
+  }
+  if (employee.reviewRating === 3) {
+    console.log("ok job, 3%");
+    bonus = 0.04;
+  }
+  if (employee.employeeNumber.length === 4) {
+    console.log("you've been here a while. 5%");
+    bonus += 0.05;
+  }
+  if (employee.annualSalary > "65000") {
+    console.log("you make quite a bit. -1%");
+    bonus -= 0.01;
+  }
+  if (bonus > 0.13) {
+    console.log(`You can't get a bigger bonus than 13%!`);
+    bonus = 0.13;
+  }
+  if (bonus < 0) {
+    console.log(`You can't get a smaller bonus than 0%!`);
+    bonus = 0;
+  }
+  objOut.bonusPercentage = bonus;
+  objOut.totalCompensation = employee.annualSalary * (1 + objOut.bonusPercentage);
+  objOut.totalBonus = employee.annualSalary * objOut.bonusPercentage;
+  // set your object properties
+  return objOut;
+}
+
+console.log(bonusCalculator(employees[0]));
+console.log(bonusCalculator(employees[1]));
